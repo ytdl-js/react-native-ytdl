f9e4e09b573e966fc0bfe3812806f34b466a7d44
Abel Tesfaye <abeltesfaye45@gmail.com>
2019-09-05 12:29:04 +0300

    info-extras: minimize diff from node library

--- info-extras.js
+++ info-extras.js
@@ -1,10 +1,10 @@
-const querystring = require("query-string");
-const url = require("url");
-const Entities = require("html-entities").AllHtmlEntities;
+const qs = require('query-string');
+const url = require('url');
+const Entities = require('html-entities').AllHtmlEntities;
 
 import util from "./util";
 
-const VIDEO_URL = "https://www.youtube.com/watch?v=";
+const VIDEO_URL = 'https://www.youtube.com/watch?v=';
 const getMetaItem = (body, name) => {
   return util.between(body, `<meta itemprop="${name}" content="`, '">');
 };
@@ -15,10 +15,11 @@ const getMetaItem = (body, name) => {
  * @param {string} html
  * @return {string}
  */
-const getVideoDescription = html => {
+const getVideoDescription = (html) => {
   const regex = /<p.*?id="eow-description".*?>(.+?)<\/p>[\n\r\s]*?<\/div>/im;
   const description = html.match(regex);
-  return description ? Entities.decode(util.stripHTML(description[1])) : "";
+  return description ?
+    Entities.decode(util.stripHTML(description[1])) : '';
 };
 
 /**
@@ -27,13 +28,11 @@ const getVideoDescription = html => {
  * @param {string} body
  * @return {Object}
  */
-const getVideoMedia = body => {
-  let mediainfo = util.between(
-    body,
+const getVideoMedia = (body) => {
+  let mediainfo = util.between(body,
     '<div id="watch-description-extras">',
-    '<div id="watch-discussion" class="branded-page-box yt-card">'
-  );
-  if (mediainfo === "") {
+    '<div id="watch-discussion" class="branded-page-box yt-card">');
+  if (mediainfo === '') {
     return {};
   }
 
@@ -50,10 +49,7 @@ const getVideoMedia = body => {
   let match;
   while ((match = regexp.exec(mediainfo)) != null) {
     let [, key, value, detail] = match;
-    key = Entities.decode(key)
-      .trim()
-      .replace(/\s/g, "_")
-      .toLowerCase();
+    key = Entities.decode(key).trim().replace(/\s/g, '_').toLowerCase();
     const content = contentRegexp.exec(value);
     if (content) {
       let [, year, mediaUrl, value2] = content;
@@ -63,10 +59,10 @@ const getVideoMedia = body => {
         media.year = parseInt(detail);
       }
       value = value.slice(0, content.index);
-      if (key !== "game" || value2 !== "YouTube Gaming") {
+      if (key !== 'game' || value2 !== 'YouTube Gaming') {
         value += value2;
       }
-      media[key + "_url"] = "";
+      media[key + '_url'] = url.resolve(VIDEO_URL, mediaUrl);
     }
     media[key] = Entities.decode(value);
   }
@@ -81,45 +77,32 @@ const getVideoMedia = body => {
  */
 const userRegexp = /<a href="\/user\/([^"]+)/;
 const verifiedRegexp = /<span .*?(aria-label="Verified")(.*?(?=<\/span>))/;
-const getAuthor = body => {
-  let ownerinfo = util.between(
-    body,
+const getAuthor = (body) => {
+  let ownerinfo = util.between(body,
     '<div id="watch7-user-header" class=" spf-link ">',
-    '<div id="watch8-action-buttons" class="watch-action-buttons clearfix">'
-  );
-  if (ownerinfo === "") {
+    '<div id="watch8-action-buttons" class="watch-action-buttons clearfix">');
+  if (ownerinfo === '') {
     return {};
   }
-  const channelName = Entities.decode(
-    util.between(
-      util.between(ownerinfo, '<div class="yt-user-info">', "</div>"),
-      ">",
-      "</a>"
-    )
-  );
+  const channelName = Entities.decode(util.between(util.between(
+    ownerinfo, '<div class="yt-user-info">', '</div>'), '>', '</a>'));
   const userMatch = ownerinfo.match(userRegexp);
   const verifiedMatch = ownerinfo.match(verifiedRegexp);
-  const channelID = getMetaItem(body, "channelId");
-  const username = userMatch
-    ? userMatch[1]
-    : util.between(
-        util.between(body, '<span itemprop="author"', "</span>"),
-        "/user/",
-        '">'
-      );
+  const channelID = getMetaItem(body, 'channelId');
+  const username = userMatch ? userMatch[1] : util.between(
+        util.between(body, '<span itemprop="author"', '</span>'), '/user/', '">');
   return {
     id: channelID,
     name: channelName,
-    avatar: url.resolve(
-      VIDEO_URL,
-      util.between(ownerinfo, 'data-thumb="', '"')
-    ),
+    avatar: url.resolve(VIDEO_URL, util.between(ownerinfo,
+      'data-thumb="', '"')),
     verified: !!verifiedMatch,
     user: username,
-    channel_url: "https://www.youtube.com/channel/" + channelID,
-    user_url: "https://www.youtube.com/user/" + username
+    channel_url: 'https://www.youtube.com/channel/' + channelID,
+    user_url: 'https://www.youtube.com/user/' + username,
   };
 };
+
 
 /**
  * Get video published at from html.
@@ -127,8 +110,8 @@ const getAuthor = body => {
  * @param {string} body
  * @return {string}
  */
-const getPublished = body => {
-  return Date.parse(getMetaItem(body, "datePublished"));
+const getPublished = (body) => {
+  return Date.parse(getMetaItem(body, 'datePublished'));
 };
 
 /**
@@ -138,14 +121,14 @@ const getPublished = body => {
  * @param {string} body
  * @return {Array.<Object>}
  */
-const getRelatedVideos = body => {
-  let jsonStr = util.between(body, "'RELATED_PLAYER_ARGS': {\"rvs\":", "},");
+const getRelatedVideos = (body) => {
+  let jsonStr = util.between(body, '\'RELATED_PLAYER_ARGS\': {"rvs":', '},');
   try {
     jsonStr = JSON.parse(jsonStr);
   } catch (err) {
     return [];
   }
-  return jsonStr.split(",").map(link => querystring.parse(link));
+  return jsonStr.split(',').map((link) => qs.parse(link));
 };
 
 let extras = {
