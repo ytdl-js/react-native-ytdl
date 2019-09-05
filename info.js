c190c8e17b8f93888c0a3e72b1ede4b813b72614
Abel Tesfaye <abeltesfaye45@gmail.com>
2019-09-05 14:08:15 +0300

    info: minimize diff from node library

--- info.js
+++ info.js
@@ -1,16 +1,21 @@
-const querystring = require("querystring");
+const querystring = require('querystring');
 
-import sig from "./sig";
-import util from "./util";
-import extras from "./info-extras";
-import FORMATS from "./formats";
+import sig from './sig';
+import util from './util';
+import extras from './info-extras';
+import FORMATS from './formats';
 
-const VIDEO_URL = "https://www.youtube.com/watch?v=";
-const EMBED_URL = "https://www.youtube.com/embed/";
-const VIDEO_EURL = "https://youtube.googleapis.com/v/";
-const INFO_HOST = "www.youtube.com";
-const INFO_PATH = "/get_video_info";
-const KEYS_TO_SPLIT = ["keywords", "fmt_list", "fexp", "watermark"];
+const VIDEO_URL = 'https://www.youtube.com/watch?v=';
+const EMBED_URL = 'https://www.youtube.com/embed/';
+const VIDEO_EURL = 'https://youtube.googleapis.com/v/';
+const INFO_HOST = 'www.youtube.com';
+const INFO_PATH = '/get_video_info';
+const KEYS_TO_SPLIT = [
+   'fmt_list',
+    'fexp',
+    'watermark'
+  ];
+
 
 /**
  * Gets info from a video without getting additional formats.
@@ -21,43 +26,28 @@ const KEYS_TO_SPLIT = ["keywords", "fmt_list", "fexp", "watermark"];
  */
 const getBasicInfo = (id, options, callback) => {
   // Try getting config from the video page first.
-  const params = "hl=" + (options.lang || "en");
-  let url =
-    VIDEO_URL + id + "&" + params + "&bpctr=" + Math.ceil(Date.now() / 1000);
+  const params = 'hl=' + (options.lang || 'en');
+  let url = VIDEO_URL + id + '&' + params +
+   '&bpctr=' + Math.ceil(Date.now() / 1000);
 
   // Remove header from watch page request.
   // Otherwise, it'll use a different framework for rendering content.
   const reqOptions = Object.assign({}, options.requestOptions);
   reqOptions.headers = Object.assign({}, reqOptions.headers, {
-    "User-Agent": ""
+    'User-Agent': ''
   });
 
   fetch(url)
     .then(body => body.text())
     .then(body => {
       // Check if there are any errors with this video page.
-      const unavailableMsg = util.between(
-        body,
-        '<div id="player-unavailable"',
-        ">"
-      );
-      if (
-        unavailableMsg &&
-        !/\bhid\b/.test(util.between(unavailableMsg, 'class="', '"'))
-      ) {
+      const unavailableMsg = util.between(body, '<div id="player-unavailable"', '>');
+      if (unavailableMsg &&
+        !/\bhid\b/.test(util.between(unavailableMsg, 'class="', '"'))) {
         // Ignore error about age restriction.
         if (!body.includes('<div id="watch7-player-age-gate-content"')) {
-          return callback(
-            Error(
-              util
-                .between(
-                  body,
-                  '<h1 id="unavailable-message" class="message">',
-                  "</h1>"
-                )
-                .trim()
-            )
-          );
+          return callback(Error(util.between(body,
+                  '<h1 id="unavailable-message" class="message">', '</h1>').trim()));
         }
       }
 
@@ -77,29 +67,23 @@ const getBasicInfo = (id, options, callback) => {
 
         // Get related videos.
         related_videos: extras.getRelatedVideos(body),
-
-        // Give the standard link to the video.
-        video_url: VIDEO_URL + id
       };
 
-      const jsonStr = util.between(body, "ytplayer.config = ", "</script>");
+      const jsonStr = util.between(body, 'ytplayer.config = ', '</script>');
       let config;
       if (jsonStr) {
-        config = jsonStr.slice(0, jsonStr.lastIndexOf(";ytplayer.load"));
+        config = jsonStr.slice(0, jsonStr.lastIndexOf(';ytplayer.load'));
         gotConfig(id, options, additional, config, false, callback);
+     
       } else {
         // If the video page doesn't work, maybe because it has mature content.
         // and requires an account logged in to view, try the embed page.
-        url = EMBED_URL + id + "?" + params;
+        url = EMBED_URL + id + '?' + params;
 
         fetch(url)
           .then(body => body.text())
           .then(body => {
-            config = util.between(
-              body,
-              "t.setConfig({'PLAYER_CONFIG': ",
-              /\}(,'|\}\);)/
-            );
+            config = util.between(body, 't.setConfig({\'PLAYER_CONFIG\': ', /\}(,'|\}\);)/);
             gotConfig(id, options, additional, config, true, callback);
           })
           .catch(err => {
@@ -112,6 +96,26 @@ const getBasicInfo = (id, options, callback) => {
     });
 };
 
+
+/**
+ * @param {Object} info
+ * @return {Array.<Object>}
+ */
+const parseFormats = (info) => {
+  let formats = [];
+  if (info.url_encoded_fmt_stream_map) {
+    formats = formats.concat(info.url_encoded_fmt_stream_map.split(','));
+  }
+  if (info.adaptive_fmts) {
+    formats = formats.concat(info.adaptive_fmts.split(','));
+  }
+  formats = formats.map((format) => querystring.parse(format));
+  delete info.url_encoded_fmt_stream_map;
+  delete info.adaptive_fmts;
+  return formats;
+};
+
+
 /**
  * @param {Object} id
  * @param {Object} options
@@ -122,75 +126,66 @@ const getBasicInfo = (id, options, callback) => {
  */
 const gotConfig = (id, options, additional, config, fromEmbed, callback) => {
   if (!config) {
-    return callback(Error("Could not find player config"));
+    return callback(Error('Could not find player config'));
   }
   try {
-    config = JSON.parse(config + (fromEmbed ? "}" : ""));
+    config = JSON.parse(config + (fromEmbed ? '}' : ''));
   } catch (err) {
-    return callback(Error("Error parsing config: " + err.message));
+    return callback(Error('Error parsing config: ' + err.message));
   }
 
   fetch(
-    "https://" +
+    'https://' +
       INFO_HOST +
       INFO_PATH +
-      "?" +
+      '?' +
       querystring.stringify({
         video_id: id,
         eurl: VIDEO_EURL + id,
-        ps: "default",
-        gl: "US",
-        hl: "en",
-        sts: config.sts
+        ps: 'default',
+        gl: 'US',
+        hl: (options.lang || 'en'),
+        sts: config.sts,
       })
   )
     .then(body => body.text())
     .then(body => {
       let info = querystring.parse(body);
 
-      if (info.status === "fail") {
-        if (
-          config.args &&
-          (config.args.fmt_list ||
-            config.args.url_encoded_fmt_stream_map ||
-            config.args.adaptive_fmts)
-        ) {
-          info = config.args;
-          info.no_embed_allowed = true;
-        } else {
+      if (!info.url_encoded_fmt_stream_map && !info.adaptive_fmts &&
+        !info.config && (config.args.fmt_list ||
+        config.args.url_encoded_fmt_stream_map || config.args.adaptive_fmts)) {
+        info = config.args;
+        info.no_embed_allowed = true;
+      } else if (info.status === 'fail') {
           return callback(
-            Error(`Code ${info.errorcode}: ${util.stripHTML(info.reason)}`)
-          );
+            Error(`Code ${info.errorcode}: ${util.stripHTML(info.reason)}`));
         }
-      }
 
-      const player_response =
-        config.args.player_response || info.player_response;
-      if (player_response) {
+      const player_response = config.args.player_response || info.player_response;
         try {
           info.player_response = JSON.parse(player_response);
         } catch (err) {
           return callback(
-            Error("Error parsing `player_response`: " + err.message)
-          );
+            Error('Error parsing `player_response`: ' + err.message));
         }
         let playability = info.player_response.playabilityStatus;
-        if (playability && playability.status === "UNPLAYABLE") {
+        if (playability && playability.status === 'UNPLAYABLE') {
           return callback(Error(playability.reason));
         }
-      }
 
       // Split some keys by commas.
-      KEYS_TO_SPLIT.forEach(key => {
+      KEYS_TO_SPLIT.forEach((key) => {
         if (!info[key]) return;
-        info[key] = info[key].split(",").filter(v => v !== "");
+        info[key] = info[key]
+          .split(',')
+          .filter((v) => v !== '');
       });
 
-      info.fmt_list = info.fmt_list
-        ? info.fmt_list.map(format => format.split("/"))
-        : [];
+      info.fmt_list = info.fmt_list ?
+        info.fmt_list.map((format) => format.split('/')) : [];
 
-      info.formats = util.parseFormats(info);
+      info.formats = parseFormats(info);
 
       // Add additional properties to info.
       Object.assign(info, additional);
@@ -207,6 +202,7 @@ const gotConfig = (id, options, additional, config, fromEmbed, callback) => {
     });
 };
 
+
 /**
  * Gets info from a video additional formats and deciphered URLs.
  *
@@ -217,50 +213,35 @@ const gotConfig = (id, options, additional, config, fromEmbed, callback) => {
 const getFullInfo = (id, options, callback) => {
   return getBasicInfo(id, options, (err, info) => {
     if (err) return callback(err);
-
-    if (info.formats.length || info.dashmpd || info.dashmpd2 || info.hlsvp) {
-      const html5playerfile = "https://" + INFO_HOST + info.html5player;
+    const hasManifest =
+      info.player_response && info.player_response.streamingData && (
+        info.player_response.streamingData.dashManifestUrl ||
+        info.player_response.streamingData.hlsManifestUrl
+      );
+    if (info.formats.length || hasManifest) {
+      const html5playerfile = 'https://' + INFO_HOST + info.html5player;
 
       sig.getTokens(html5playerfile, options, (err, tokens) => {
         if (err) return callback(err);
 
         sig.decipherFormats(info.formats, tokens, options.debug);
-
         let funcs = [];
-
-        if (info.dashmpd) {
-          info.dashmpd = decipherURL(info.dashmpd, tokens);
-          funcs.push(getDashManifest.bind(null, info.dashmpd, options));
+        if (hasManifest && info.player_response.streamingData.dashManifestUrl) {
+          let url = info.player_response.streamingData.dashManifestUrl;
+          funcs.push(getDashManifest.bind(null, url, options));
         }
-
-        if (info.dashmpd2) {
-          info.dashmpd2 = decipherURL(info.dashmpd2, tokens);
-          funcs.push(getDashManifest.bind(null, info.dashmpd2, options));
-        }
-
-        if (info.hlsvp) {
-          info.hlsvp = decipherURL(info.hlsvp, tokens);
-          funcs.push(getM3U8.bind(null, info.hlsvp, options));
+        if (hasManifest && info.player_response.streamingData.hlsManifestUrl) {
+          let url = info.player_response.streamingData.hlsManifestUrl;
+          funcs.push(getM3U8.bind(null, url, options));
         }
 
         util.parallel(funcs, (err, results) => {
           if (err) return callback(err);
-          if (results[0]) {
-            mergeFormats(info, results[0]);
-          }
-          if (results[1]) {
-            mergeFormats(info, results[1]);
-          }
-          if (results[2]) {
-            mergeFormats(info, results[2]);
-          }
-          if (!info.formats.length) {
-            callback(Error("No formats found"));
-            return;
-          }
+          if (results[0]) { mergeFormats(info, results[0]); }
+          if (results[1]) { mergeFormats(info, results[1]); }
 
           if (options.debug) {
-            info.formats.forEach(format => {
+            info.formats.forEach((format) => {
               const itag = format.itag;
               if (!FORMATS[itag]) {
                 console.warn(`No format metadata for itag ${itag} found`);
@@ -275,20 +256,11 @@ const getFullInfo = (id, options, callback) => {
         });
       });
     } else {
-      callback(Error("This video is unavailable"));
+      callback(Error('This video is unavailable'));
     }
   });
 };
 
-/**
- * @param {string} url
- * @param {Array.<string>} tokens
- */
-const decipherURL = (url, tokens) => {
-  return url.replace(/\/s\/([a-fA-F0-9.]+)/, (_, s) => {
-    return "/signature/" + sig.decipher(tokens, s);
-  });
-};
 
 /**
  * Merges formats from DASH or M3U8 with formats from video info page.
@@ -297,19 +269,19 @@ const decipherURL = (url, tokens) => {
  * @param {Object} formatsMap
  */
 const mergeFormats = (info, formatsMap) => {
-  info.formats.forEach(f => {
+  info.formats.forEach((f) => {
     if (!formatsMap[f.itag]) {
       formatsMap[f.itag] = f;
     }
   });
   info.formats = [];
-  for (let itag in formatsMap) {
-    info.formats.push(formatsMap[itag]);
-  }
+  for (let itag in formatsMap) { info.formats.push(formatsMap[itag]); }
 };
 
+
+
 //TODO: Find a way to implement getDashManifest function, dont forget to add comments afterwards
-const getDashManifest = () => console.error("react-native-ytdl: You tried to call an unimplemented function [getDashManifest] ")
+const getDashManifest = () => console.error('react-native-ytdl: You tried to call an unimplemented function [getDashManifest] ')
 
 /**
  * Gets additional formats.
@@ -319,15 +291,15 @@ const getDashManifest = () => console.error("react-native-ytdl: You tried to cal
  * @param {Function(!Error, Array.<Object>)} callback
  */
 const getM3U8 = (url, options, callback) => {
-  url = "https://" + INFO_HOST + url;
+  url = 'https://' + INFO_HOST + url;
   request(url, options.requestOptions, (err, res, body) => {
     if (err) return callback(err);
 
     let formats = {};
     body
-      .split("\n")
-      .filter(line => /https?:\/\//.test(line))
-      .forEach(line => {
+      .split('\n')
+      .filter((line) => /https?:\/\//.test(line))
+      .forEach((line) => {
         const itag = line.match(/\/itag\/(\d+)\//)[1];
         formats[itag] = { itag: itag, url: line };
       });
@@ -335,5 +307,5 @@ const getM3U8 = (url, options, callback) => {
   });
 };
 
-let info = { getBasicInfo, gotConfig, getFullInfo };
+const info = { getBasicInfo, gotConfig, getFullInfo };
 export default info;
