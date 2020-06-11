3e3e7be743ab37c2b267b7dc47ba4113363321fc
Abel Tesfaye <abeltesfaye45@gmail.com>
2020-06-11 15:13:06 +0300

    code: rebase to node-ytdl-core v3.1.1

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-import getInfo from "./info";
-import util from "./util";
-import sig from "./sig";
+import ytdl from './lib';
 
-
-exports.getBasicInfo = getInfo.getBasicInfo;
-exports.getInfo = getInfo.getFullInfo;
-exports.chooseFormat = util.chooseFormat;
-exports.filterFormats = util.filterFormats;
-exports.validateID = util.validateID;
-exports.validateURL = util.validateURL;
-exports.getURLVideoID = util.getURLVideoID;
-exports.getVideoID = util.getVideoID;
-exports.cache = {
-  sig: sig.cache,
-  info: getInfo.cache,
-};
-
-export default exports;
+export default ytdl;
